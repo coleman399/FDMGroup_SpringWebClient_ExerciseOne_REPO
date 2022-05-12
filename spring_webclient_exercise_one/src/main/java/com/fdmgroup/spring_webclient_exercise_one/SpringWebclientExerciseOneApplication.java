0078895370cdac18400793caccfd7a1a618954a2
dillon.coleman <coleman399@gmail.com>
2022-05-12 07:04:53 -0700

    init commit

---
+++ spring_webclient_exercise_one/src/main/java/com/fdmgroup/spring_webclient_exercise_one/SpringWebclientExerciseOneApplication.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.spring_webclient_exercise_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebclientExerciseOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebclientExerciseOneApplication.class, args);
+	}
+
+}
---
+++ spring_webclient_exercise_one/src/test/java/com/fdmgroup/spring_webclient_exercise_one/SpringWebclientExerciseOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.spring_webclient_exercise_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebclientExerciseOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
